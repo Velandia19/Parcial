f1f9e91da9b23ff431e91ea5eeb31666900dc32c
Gissela Velandia <jeimyg.velandiag@uqvirtual.edu.co>
2022-05-10 19:45:33 -0500

    Cambios para observar el ocrreo electronico

--- comandos.py
+++ comandos.py
@@ -1,5 +1,10 @@
+from statistics import correlation
+
+
 nxisndkmsf,ld,lf,glommofmomfomvofvo,fol,vcdcdc
 vcndjcnjdnckdc
 vfmvnfvknckv
 c kcdkvkdv
 fcdf
+se realizaron cambios en el texto, para agregar un commit y enviar al correo electronico. idncndcnjmnxjnjn
+cambios realizados paea poder visualizar el correlation
